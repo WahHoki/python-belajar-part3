e1fb5e0b2427aa047549d081bedd13a27b6d57f7
Hoki <wahhoki07@gmail.com>
2025-05-28 17:41:39 +0800

    Create 8.dictionary.py

---
+++ 8.dictionary.py
@@ -0,0 +1,20 @@
+# list -> array, dimana kita mengakses data dengan mengguanakan index
+
+data_list = ['ucup','otong','dadang']
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+    'key':'value',
+    'cp':'ucup',
+    'tg':'otong',
+    'dg':'dadang',
+    'nbr':100,
+    'list':data_list,
+}
+print(data_dict['tg'])
+print(data_dict['nbr'])
+print(data_dict['list'])

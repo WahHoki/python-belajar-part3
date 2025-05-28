42a1572d580cbadac23eb5061a03eaff800d4c53
Hoki <wahhoki07@gmail.com>
2025-05-28 17:41:09 +0800

    Create 7.tupleandset.py

---
+++ 7.tupleandset.py
@@ -0,0 +1,17 @@
+# list
+
+data_list = [10,2,4,3,2] # menggunakan kurung siku
+print(data_list)
+# tuples
+data_tuples = (7,8,9,10)
+print(data_tuples)
+print(data_tuples[1])
+
+# data_tuples[1] = "ucup"  ->(ini tidak bisa dilakukan karena tidak bisa dirubah)
+# data_tuples.append(1) ->(data tuple tidak bisa dirubah)
+
+# sets (himpunan)
+data_sets = {10,4,3,2,4,7,6,5}
+print(data_sets)
+
+print(data_sets[0])
